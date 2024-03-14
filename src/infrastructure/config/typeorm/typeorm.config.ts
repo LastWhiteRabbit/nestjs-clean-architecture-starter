38a4814ea62e53baab1a77e33b49ccaf7a1863d1
LastWhiteRabbit <elvedingran@gmail.com>
2024-03-14 11:06:03 +0100

    fix:add env bugfix - linted

--- src/infrastructure/config/typeorm/typeorm.config.ts
+++ src/infrastructure/config/typeorm/typeorm.config.ts
@@ -2,7 +2,7 @@ import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 
 if (process.env.NODE_ENV === 'local') {
-  dotenv.config({ path: './env/local.env'  });
+  dotenv.config({ path: './env/local.env' });
 }
 
 const config = new DataSource({
